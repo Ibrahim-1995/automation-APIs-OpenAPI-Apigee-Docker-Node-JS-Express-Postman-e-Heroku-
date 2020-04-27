97b32b6e971b967aff3b01b49a57069e69494412
rosedias <rosedias.info@gmail.com>
2020-04-27 13:29:01 -0300

    Model

---
+++ api-criacao-conta-usuario/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+import accountModel from "./model/accountModel";
+
+const app = express();
+mongoose.connect(process.env.MONGODB_URI);
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Erro de conexão"))
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
+
+
